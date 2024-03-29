c9c40e45be2506b009997b3d1e9368e105c750f0
Harsh saini <harshbhagirath292@gmail.com>
2024-03-29 12:44:54 +0530

    modify appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     <div className="border-bottom border-2">
     <Header/>
     </div>
-    
       <div className="container-sm">100% wide until small breakpoint</div>
       <div className="container-md">100% wide until medium breakpoint</div>
     </>
