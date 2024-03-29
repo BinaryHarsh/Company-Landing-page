5358919dcf6c0346c5f54cac972a1c8ca15767da
Harsh saini <harshbhagirath292@gmail.com>
2024-03-29 12:39:59 +0530

    appjsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,6 @@ function App() {
     
       <div className="container-sm">100% wide until small breakpoint</div>
       <div className="container-md">100% wide until medium breakpoint</div>
-      <div className="container-lg">100% wide until large breakpoint</div>
-      <div className="container-xl">100% wide until extra large breakpoint</div>
-      <div className="container-xxl">
-        100% wide until extra extra large breakpoint
-      </div>
     </>
   );
 }
